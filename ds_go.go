7e5414a18258a4da4b62ee6495639677789d9b78
emre@ozudogru.com <Gnn1207yy>
2019-03-27 12:04:43 +0300

    Initial

---
+++ ds_go.go
@@ -0,0 +1,41 @@
+package domainsearch_go
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/haccer/available"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("Türkçe sözlükteki kelimelerin, com ve net uzantılı alan adlarının boşta olup olmadığı kontrol ediliyor...")
+	file, err := os.Open("wtzl.txt")
+
+	if err != nil {
+		log.Fatalf("failed opening file: %s", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+	var txtlines []string
+
+	for scanner.Scan() {
+		txtlines = append(txtlines, scanner.Text())
+	}
+
+	file.Close()
+	ext := []string{".us"}
+
+	for _, word := range txtlines {
+		//		for i := 0; i < len(ext); i++ {
+		for _, tld := range ext {
+			domain := word + tld
+			available := available.Domain(domain)
+			fmt.Println(domain + " looking...")
+			if available {
+				fmt.Println(domain + " alan adı şu an için boşta.")
+			}
+		}
+	}
+}
