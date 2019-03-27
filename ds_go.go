9b44c1db01c6168b3971a8663df910fb52da6f3b
Emre Ozudogru <emre@ozudogru.com>
2019-03-27 15:28:03 +0300

    Initial

--- ds_go.go
+++ ds_go.go
@@ -9,32 +9,26 @@ import (
 )
 
 func main() {
-	fmt.Println("Türkçe sözlükteki kelimelerin, com ve net uzantılı alan adlarının boşta olup olmadığı kontrol ediliyor...")
+	fmt.Println("Loading wordlist and looking for available domains")
 	file, err := os.Open("wtzl.txt")
-
 	if err != nil {
 		log.Fatalf("failed opening file: %s", err)
 	}
-
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 	var txtlines []string
-
 	for scanner.Scan() {
 		txtlines = append(txtlines, scanner.Text())
 	}
-
 	file.Close()
 	ext := []string{".us"}
-
 	for _, word := range txtlines {
-		//		for i := 0; i < len(ext); i++ {
 		for _, tld := range ext {
 			domain := word + tld
 			available := available.Domain(domain)
 			fmt.Println(domain + " looking...")
 			if available {
-				fmt.Println(domain + " alan adı şu an için boşta.")
+				fmt.Println(domain + " is EMPTY")
 			}
 		}
 	}
